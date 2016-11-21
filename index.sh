58128f413df355eace8e0f7665c1699e67d6dc60
feizheng <1290657123@qq.com>
2016-11-21 13:43:29 +0800

    update deps

--- index.sh
+++ index.sh
@@ -15,13 +15,13 @@ alias apache-start='service httpd start';
 alias apache-stop='service httpd stop';
 
 ## maria-start:
-alias mysql-start='service mariadb start';
-alias mysql-stop='service mariadb stop';
-alias mysql-status='service mariadb status';
-alias mysql-restart='service mariadb restart';
-alias mysql-resetpwd='mysql_secure_installation';
-alias mysql-data='cd /var/lib/mysql';
-alias mysql-login='mysql -u root -p';
+# alias mysql-start='service mariadb start';
+# alias mysql-stop='service mariadb stop';
+# alias mysql-status='service mariadb status';
+# alias mysql-restart='service mariadb restart';
+# alias mysql-resetpwd='mysql_secure_installation';
+# alias mysql-data='cd /var/lib/mysql';
+# alias mysql-login='mysql -u root -p';
 
 alias tf-access-log='tail -f /etc/httpd/logs/access_log';
 
